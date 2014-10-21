1e7d5bac9b9a6b743473420132033c973dd91d97
sebbrochet <contact@sebbrochet.com>
2014-10-21 11:31:25 +0200

    add_to_inventory OK but need more testing

---
+++ README.md
@@ -0,0 +1,64 @@
+## vminventory: vCenter inventory update from .vmx files tool 
+
+This command-line tool lets you update your vCenter inventory based on .vmx files not already linked to VMs.
+
+Requirements
+* linux box
+* Python 2.6 or higher
+* [argparse](https://docs.python.org/3/library/argparse.html) library
+* [pyvmomi](https://github.com/vmware/pyvmomi) library
+* access to a VMWare vCenter host
+
+Installation:
+-------------
+* Clone repository   
+`git clone https://github.com/sebbrochet/vminventory.git`
+* cd into project directory   
+`cd vminventory`
+* Install requirements with pip   
+`pip install -r requirements.txt`
+* Install vminventory binary   
+`python setup.py install`
+
+Usage:
+------
+
+```
+usage: vminventory [-h] [-s SCOPE] [-u USER] [-p PASSWORD] [-t TARGET]
+                   [-o PORT] [-r RESOURCEPOOL] [-d DATACENTER] [-c CONFIG]
+                   [-v]
+                   command
+
+vCenter inventory update from .vmx files tool.
+
+positional arguments:
+  command               Command to execute (add_to_inventory,
+                        list_orphaned_vm, list_resourcepool)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SCOPE, --scope SCOPE
+                        Limit command to act on scope defined in a file
+  -u USER, --user USER  Specify the user account to use to connect to vCenter
+  -p PASSWORD, --password PASSWORD
+                        Specify the password associated with the user account
+  -t TARGET, --target TARGET
+                        Specify the vCenter host to connect to
+  -o PORT, --port PORT  Port to connect on (default is 443)
+  -r RESOURCEPOOL, --resourcepool RESOURCEPOOL
+                        Specify the name of the resource pool to add VM in
+                        (default is 1st one)
+  -d DATACENTER, --datacenter DATACENTER
+                        Specify the datacenter name to run commands on
+                        (default is all datacenters)
+  -c CONFIG, --config CONFIG
+                        Configuration file to use
+  -v, --version         Print program version and exit.
+```
+
+`scope` of VM to be listed/exported/imported can be defined in a file by listing the short VM names, one by line.   
+A line starting with a `#` is considered as comment and won't be interpreted.     
+
+`config` format is one argument by line (i.e argname=value), argument names are the same ones than the CLI (scope, user, password, ...).   
+A line starting with a `#` is considered as comment and won't be interpreted.    
+Don't put quotes between argument values
--- lib/vminventory/commands.py
+++ lib/vminventory/commands.py
@@ -28,6 +28,21 @@ def get_full_name(vm):
     return full_name
 
 
+def get_friendly_name(entity):
+    full_name = get_full_name(entity)
+
+    parts = full_name.split('/')
+
+    friendly_name = ""
+
+    if len(parts) == 5:
+        friendly_name = '%s/%s' % (parts[1], parts[3])
+    elif len(parts) == 6:
+        friendly_name = '%s/%s/%s' % (parts[1], parts[3], parts[5])
+
+    return friendly_name 
+    
+
 def get_service_instance(args):
     service_instance = None
 
@@ -97,6 +112,8 @@ def get_all_vmx_data(target, service_instance, vms_from_scope, datacenter_name):
                     vmfold = dsfolder.split("]")
                     vmfold = vmfold[1]
                     vmfold = vmfold[1:]
+                    if vmfold[-1] == '/':
+                        vmfold = vmfold[:-1]
                     vmx_data_in_datastore.append((target, vmfold, dsfile, datacenter, fulldsname))
                 except Exception, e:
                     print "Caught exception : " + str(e)
@@ -132,7 +149,7 @@ def get_all_uuid_from_vmx_data(args, all_vmx_data):
         password = args.password
 
         target, vmfold, dsfile, datacenter, fulldsname = vmx_data
-        vmx_url = "https://%s/folder/%s%s?dcPath=%s&dsName=%s" % (target, vmfold, dsfile, datacenter, fulldsname) 
+        vmx_url = "https://%s/folder/%s/%s?dcPath=%s&dsName=%s" % (target, vmfold, dsfile, datacenter, fulldsname) 
         request = urllib2.Request(url_fix(vmx_url))
         base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
         request.add_header("Authorization", "Basic %s" % base64string)
@@ -160,16 +177,16 @@ def get_all_uuid_from_vmx_data(args, all_vmx_data):
 
         except urllib2.HTTPError, e:
             print "Exception while checking %s:\n%s" % (vmx_url, e)
-            return None, "", ""
+            return None, "", "", ""
 
-        return uuid, newDN, dspath
+        return uuid, newDN, fulldsname, vmfold 
 
     uuid_from_vmx_data_dict = {}
 
     for vmx_data in all_vmx_data:
-       uuid, datastore_vm, datastore_path = get_uuid_from_vmx_data(args, vmx_data)
+       uuid, datastore_vm, fulldsname, vmfold = get_uuid_from_vmx_data(args, vmx_data)
        if uuid:
-           uuid_from_vmx_data_dict[uuid] = (datastore_vm, datastore_path) 
+           uuid_from_vmx_data_dict[uuid] = (datastore_vm, fulldsname, vmfold) 
     
     return uuid_from_vmx_data_dict
 
@@ -207,25 +224,98 @@ def get_vmx_data_not_in_inventory(args):
         vmx_data_not_in_inventory.append(uuid_from_vmx_data_dict[uuid])
 
     return vmx_data_not_in_inventory
-    
+
+ 
 def cmd_list_orphaned_vm(args):
+    print "Looking for orphaned VM..."
+    
     vmx_data_not_in_inventory = get_vmx_data_not_in_inventory(args)
 
     if vmx_data_not_in_inventory:
-        print "Orphaned VM found (Display name - Datastore/Folder name)"
-        for datastore_vm, datastore_path in vmx_data_not_in_inventory:
-            print "%s - %s" % (datastore_vm, datastore_path)
+        print "Orphaned VM found (Display name - [Datastore] Folder name)"
+        for datastore_vm, fulldsname, vmfold in vmx_data_not_in_inventory:
+            print "%s - [%s] %s" % (datastore_vm, fulldsname, vmfold)
     else:
         print "No orphaned VM found."
 
 
+def get_resource_pool_IFP(args):
+    service_instance = get_service_instance(args)
+    content = service_instance.RetrieveContent()
+    object_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.ResourcePool], True)
+
+    resourcepool = None
+
+    for current_resourcepool in object_view.view:
+        if get_friendly_name(current_resourcepool) == args.resourcepool:
+            resourcepool = current_resourcepool
+            break
+
+    return resourcepool
+
+        
+def get_vm_folder_IFP(args):
+    service_instance = get_service_instance(args)
+    content = service_instance.RetrieveContent()
+    object_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Folder], True)
+
+    folder = content.rootFolder
+
+    for current_folder in object_view.view:
+        if 'VirtualMachine' in current_folder.childType:
+            #if current_folder ==
+            folder = current_folder
+            break
+
+    return folder
+
+
 def cmd_add_to_inventory(args):
+    if not args.resourcepool:
+        print "Error: resourcepool parameter is mandatory with the 'add_to_inventory' command."
+        print "You can use the 'list_resourcepool' command to display them with a friendly name."
+        return
+
+    resource_pool = get_resource_pool_IFP(args)
+
+    if not resource_pool:
+        print "Error: resourcepool '%s' not found" % args.resourcepool
+        print "You can use the 'list_resourcepool' command to display them with a friendly name."
+        return
+
+    print "Looking for orphaned VM..."
+ 
     vmx_data_not_in_inventory = get_vmx_data_not_in_inventory(args)
 
     if vmx_data_not_in_inventory:
-        for datastore_vm, datastore_path in vmx_data_not_in_inventory:
-            folder = get_datastore_folder(datastore_vm, datastore_path)
-            if folder:
-                folder.RegisterVM(path=datastore_path, asTemplate = False)
+        folder = get_vm_folder_IFP(args)
+
+        for datastore_vm, fulldsname, vmfold in vmx_data_not_in_inventory:
+            datastore_path = "[%s] %s/%s.vmx" % (fulldsname, vmfold, datastore_vm)
+            status_str = "Adding '%s' in inventory '%s': " % (datastore_path, get_full_name(folder))
+            print status_str,
+
+            import sys
+            sys.stdout.flush()
+
+            task = folder.RegisterVM_Task(path=datastore_path, asTemplate = False, pool=resource_pool)
+
+            import time
+            while task.info.state == vim.TaskInfo.State.running:
+                time.sleep(0.1)
+
+            if task.info.state == vim.TaskInfo.State.success:
+                print "OK"
+            else:
+                print "NOK"
     else:
         print "No vmx files to add in inventory."     
+
+def cmd_list_resourcepool(args):
+    service_instance = get_service_instance(args)
+    content = service_instance.RetrieveContent()
+    object_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.ResourcePool], True)
+
+    for resourcepool in object_view.view:
+        if is_in_datacenter(resourcepool, args.datacenter): 
+            print get_friendly_name(resourcepool)
